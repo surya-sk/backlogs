2df94df2110a1c737a2c1a1d88408de51e219918
Surya Kashyap <surya.sk05@outlook.com>
2021-10-14 20:47:01 -0300

    Create class for logging

---
+++ backlog/Utils/Logger.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace backlog.Utils
+{
+    public static class Logger
+    {
+        static StorageFolder localFolder = ApplicationData.Current.LocalFolder;
+        static string fileName = "logs.txt";
+
+        public static async Task WriteLogAsync(string log)
+        {
+            var file = await localFolder.CreateFileAsync(fileName, CreationCollisionOption.OpenIfExists);
+            await FileIO.AppendTextAsync(file, log);
+        }
+
+        public static async Task<String> ReadLogAsync()
+        {
+            var file = await localFolder.GetFileAsync(fileName);
+            string logs = await FileIO.ReadTextAsync(file);
+            return logs;
+        }
+    }
+}
