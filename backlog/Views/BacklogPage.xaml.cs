c1ecbd0bb66a1350e445907a1f83266e1862b4e9
surya-sk <surya.sk05@outlook.com>
2022-01-21 18:22:20 -0400

    Update date formatting

--- backlog/Views/BacklogPage.xaml.cs
+++ backlog/Views/BacklogPage.xaml.cs
@@ -177,7 +177,7 @@ namespace backlog.Views
             await Logger.Info("Marking backlog as complete");
             backlog.IsComplete = true;
             backlog.UserRating = UserRating.Value;
-            backlog.CompletedDate = DateTimeOffset.Now.Date.ToString("d", CultureInfo.InvariantCulture);
+            backlog.CompletedDate = DateTimeOffset.Now.Date.ToString("D", CultureInfo.InvariantCulture);
             await SaveBacklog();
             RatingDialog.Hide();
             Frame.Navigate(typeof(MainPage));
