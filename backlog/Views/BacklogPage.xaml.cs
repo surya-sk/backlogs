178cea3b1aee9754bedab5a4ab3a0641adc96d92
surya-sk <surya.sk05@outlook.com>
2022-02-22 08:25:56 -0400

    Show completed date for completed backlogs

--- backlog/Views/BacklogPage.xaml.cs
+++ backlog/Views/BacklogPage.xaml.cs
@@ -183,7 +183,7 @@ namespace backlog.Views
             await Logger.Info("Marking backlog as complete");
             backlog.IsComplete = true;
             backlog.UserRating = UserRating.Value;
-            backlog.CompletedDate = DateTimeOffset.Now.Date.ToString("D", CultureInfo.InvariantCulture);
+            backlog.CompletedDate = DateTimeOffset.Now.Date.ToString("d", CultureInfo.InvariantCulture);
             await SaveBacklog();
             RatingDialog.Hide();
             Frame.Navigate(typeof(MainPage));
