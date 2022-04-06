f66511c430585a8fcb8100d472490fd6df9c6920
surya-sk <surya.sk05@outlook.com>
2022-04-06 20:36:02 -0300

    Update changelog

--- backlog/Views/SettingsPage.xaml.cs
+++ backlog/Views/SettingsPage.xaml.cs
@@ -27,15 +27,8 @@ namespace backlog.Views
         static string MIT_LICENSE = "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: \n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. \n\nTHE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
         static string GNU_LICENSE = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. \n\nYou should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/";
         static string CHANGE_LOG = "\u2022 Notifications are now working again\n" +
-            "\u2022 Fix an issue causing Backlogs to not sync automatically after sign-in\n" +
-            "\u2022 Live tile will now be updated after creating a Backlog without having to Sync\n" +
-            "\u2022 The Stats and Suggestions sections on the homepage will now be displayed on the right to cover the whole screen when the window size is large\n" +
-            "\u2022 The homepage now shows Backlogs that are in progress\n" +
-            "\u2022 The Backlogs page now has an option to random generate a pick based on which type of Backlog you are currently viewing\n" +
-            "\u2022 Fixed a crash caused when no file is selected when importing a backlog\n" +
-            "\u2022 Changed order of sections in the Setting page\n" +
-            "\u2022 Logs can now be viewed directly instead of opening the logs folder\n" +
-            "\u2022 Settings page now shows a changelog :)";
+            "\u2022 Fixed vertical scrolling not working on the homepage\n" +
+            "\u2022 The Create page will now automatically select Backlog type based on what section the user was in\n";
         
         public string Version
         {
