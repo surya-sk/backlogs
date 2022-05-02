18b79c4fd49e90d496bffff7d65ed4b45cab1e44
surya-sk <surya.sk05@outlook.com>
2022-05-02 18:24:53 -0300

    Update changelog

--- backlog/Views/SettingsPage.xaml.cs
+++ backlog/Views/SettingsPage.xaml.cs
@@ -28,7 +28,7 @@ namespace backlog.Views
         static string GNU_LICENSE = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. \n\nYou should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/";
         static string CHANGE_LOG = "\u2022 Removed redundant Home button on Settings page\n" +
             "\u2022 Fixed game backlogs not working because of expired Twitch tokens\n" +
-            "\u2022 You can now sign out by clicking the profile icon on the homepage\n";
+            "\u2022 You can now sign out by clicking the profile icon on the homepage";
         
         public string Version
         {
