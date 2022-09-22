0d015b0b935bb0e819afa87e10a53a0c8cc88066
surya-sk <surya.sk05@outlook.com>
2022-09-22 12:22:32 +0530

    Refactor settings viewmodel

--- backlog/ViewModels/SettingsViewModel.cs
+++ backlog/ViewModels/SettingsViewModel.cs
@@ -12,7 +12,6 @@ using Windows.ApplicationModel.Email;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml;
 using MvvmHelpers.Commands;
-using System.Security.Cryptography.X509Certificates;
 using System.ComponentModel;
 using System.Windows.Input;
 
@@ -20,6 +19,7 @@ namespace backlog.ViewModels
 {
     public class SettingsViewModel : INotifyPropertyChanged
     {
+
         private string _selectedTheme = Settings.AppTheme;
         private int _selectedTileStyleIndex = Settings.TileStyle == "Peeking" ? 0 : 1;
         private string _tileStylePreviewImage = Settings.TileStyle == "Peeking" ? "ms-appx:///Assets/peeking-tile.png" :
@@ -27,6 +27,16 @@ namespace backlog.ViewModels
         private bool _showProgress;
         private string _tileContent = Settings.TileContent;
 
+
+        public string MIT { get; } = "Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: \n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. \n\nTHE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.";
+        public string GPL { get; } = "This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. \n\nYou should have received a copy of the GNU General Public License along with this program. If not, see https://www.gnu.org/licenses/";
+        public string Changelog { get; } = "\u2022 Film, TV and game backlogs now have a button to play the trailer.\n" +
+            "\u2022 There is now a button that opens web results for the backlog.\n" +
+            "\u2022 Today's date is now the minimum date when picking target date.\n" +
+            "\u2022 The homepage now shows upcoming backlogs.\n" +
+            "\u2022 The app can now show upcoming backlogs in the live tile.\n";
+        public string ChangelogTitle { get; } = "New this version - 30 July, 2022";
+        public string Version { get; } = Settings.Version;
         public delegate void NavigateToMainPage();
 
         public NavigateToMainPage NavigateToMainPageFunc;
