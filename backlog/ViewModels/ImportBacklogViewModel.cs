4dbc5e1d9c250dd759c137832f486e7b54cf9d98
surya-sk <surya.sk05@outlook.com>
2022-11-09 12:44:28 +0530

    Move import page to view model

---
+++ backlog/ViewModels/ImportBacklogViewModel.cs
@@ -0,0 +1,255 @@
+﻿using backlog.Models;
+using backlog.Saving;
+using backlog.Utils;
+using backlog.Views;
+using Microsoft.Graph;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml;
+using System.Globalization;
+using Windows.UI.Xaml.Controls;
+using System.Windows.Input;
+using MvvmHelpers.Commands;
+
+namespace backlog.ViewModels
+{
+    public class ImportBacklogViewModel : INotifyPropertyChanged
+    {
+        private Backlog m_importedBacklog;
+        private bool m_signedIn = Settings.IsSignedIn;
+        private bool m_isNetworkAvailable;
+        private string m_fileName;
+        private bool m_isBusy;
+        private DateTimeOffset m_dateInput;
+        private TimeSpan m_notifTime;
+        private bool m_enableNotificationToggle;
+        private bool m_showNotificationToggle;
+        private bool m_showNotificationOptions;
+
+        public ICommand Import { get; set; }
+        public ICommand Cancel { get; set; }
+
+        public DateTime Today = DateTime.Today;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<Backlog> Backlogs;
+
+        public delegate void NavigateToMainPage();
+        public NavigateToMainPage NavToMainPageFunc;
+
+        public Backlog ImportedBacklog
+        {
+            get => m_importedBacklog;
+            set
+            {
+                m_importedBacklog = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ImportedBacklog)));
+            }
+        }
+
+        public string FileName
+        {
+            get => m_fileName;
+            set
+            {
+                m_fileName = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(FileName)));
+            }
+        }
+
+        public bool IsBusy
+        {
+            get => m_isBusy;
+            set
+            {
+                m_isBusy = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(IsBusy)));
+            }
+        }
+
+        public DateTimeOffset DateInput
+        {
+            get => m_dateInput;
+            set
+            {
+                m_dateInput = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DateInput)));
+                EnableNotificationToggle = m_dateInput != DateTimeOffset.MinValue;
+            }
+        }
+
+        public TimeSpan NotifTime
+        {
+            get => m_notifTime;
+            set
+            {
+                if (m_notifTime != value)
+                {
+                    m_notifTime = value;
+                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NotifTime)));
+                }
+            }
+        }
+
+        public bool EnableNotificationToggle
+        {
+            get => m_enableNotificationToggle;
+            set
+            {
+                m_enableNotificationToggle = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(EnableNotificationToggle)));
+            }
+        }
+
+        public bool ShowNotificationToggle
+        {
+            get => m_showNotificationToggle;
+            set
+            {
+                m_showNotificationToggle = value;
+                if (m_showNotificationToggle)
+                {
+                    ShowNotificationOptions = true;
+                }
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShowNotificationToggle)));
+            }
+        }
+
+        public bool ShowNotificationOptions
+        {
+            get => m_showNotificationOptions;
+            set
+            {
+                m_showNotificationOptions = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ShowNotificationOptions)));
+            }
+        }
+
+        public ImportBacklogViewModel()
+        {
+            m_isNetworkAvailable = NetworkInterface.GetIsNetworkAvailable();
+            m_importedBacklog = new Backlog();
+            m_importedBacklog.ImageURL = "https://github.com/surya-sk/backlogs/blob/master/backlog/Assets/app-icon.png"; // just a placeholder image
+
+            Import = new AsyncCommand(ImportBacklogAsync);
+            Cancel = new Command(CancelImport);
+        }
+
+        /// <summary>
+        /// Loads the json and creates a Backlog from it
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public async Task LoadBacklogFromFileAsync(string fileName)
+        {
+            if (fileName != null && fileName != "")
+            {
+                FileName = fileName;
+                IsBusy = true;
+                if (m_isNetworkAvailable)
+                {
+                    await SaveData.GetInstance().ReadDataAsync(m_signedIn);
+                    Backlogs = SaveData.GetInstance().GetBacklogs();
+                    StorageFolder tempFolder = ApplicationData.Current.TemporaryFolder;
+                    StorageFile file = await tempFolder.GetFileAsync(fileName);
+                    string json = await FileIO.ReadTextAsync(file);
+                    ImportedBacklog = JsonConvert.DeserializeObject<Backlog>(json);
+                }
+                IsBusy = false;
+            }
+        }
+
+        /// <summary>
+        /// Add imported backlog to user backlogs
+        /// </summary>
+        /// <returns></returns>
+        public async Task ImportBacklogAsync()
+        {
+            IsBusy = true;
+            if (!m_isNetworkAvailable && m_signedIn)
+            {
+                ContentDialog contentDialog = new ContentDialog
+                {
+                    Title = "No internet",
+                    Content = "You need to be connected to the internet for this!",
+                    CloseButtonText = "Ok"
+                };
+                await contentDialog.ShowAsync();
+                return;
+            }
+            if (DateInput != null && DateInput != DateTimeOffset.MinValue)
+            {
+                string date = DateInput.DateTime.ToString("D", CultureInfo.InvariantCulture);
+                if (ShowNotificationOptions)
+                {
+                    if (NotifTime == TimeSpan.Zero)
+                    {
+                        ContentDialog contentDialog = new ContentDialog
+                        {
+                            Title = "Invalid date and time",
+                            Content = "Please pick a time!",
+                            CloseButtonText = "Ok"
+                        };
+                        await contentDialog.ShowAsync();
+                        return;
+                    }
+                    DateTimeOffset dateTime = DateTimeOffset.Parse(date, CultureInfo.InvariantCulture).Add(NotifTime);
+                    int diff = DateTimeOffset.Compare(dateTime, DateTimeOffset.Now);
+                    if (diff < 0)
+                    {
+                        ContentDialog contentDialog = new ContentDialog
+                        {
+                            Title = "Invalid time",
+                            Content = "The date and time you've chosen are in the past!",
+                            CloseButtonText = "Ok"
+                        };
+                        await contentDialog.ShowAsync();
+                        return;
+                    }
+                }
+                else
+                {
+                    DateTimeOffset dateTime = DateTimeOffset.Parse(date, CultureInfo.InvariantCulture);
+                    int diff = DateTime.Compare(DateTime.Today, DateInput.DateTime);
+                    if (diff > 0)
+                    {
+                        ContentDialog contentDialog = new ContentDialog
+                        {
+                            Title = "Invalid date and time",
+                            Content = "The date and time you've chosen are in the past!",
+                            CloseButtonText = "Ok"
+                        };
+                        await contentDialog.ShowAsync();
+                        return;
+                    }
+                }
+            }
+            ImportedBacklog.id = Guid.NewGuid();
+            ImportedBacklog.UserRating = 0;
+            ImportedBacklog.Progress = 0;
+            ImportedBacklog.CompletedDate = null;
+            ImportedBacklog.IsComplete = false;
+            ImportedBacklog.CreatedDate = DateTimeOffset.Now.ToString("D", CultureInfo.InvariantCulture);
+            ImportedBacklog.TargetDate = DateInput != null ? DateInput.ToString("D", CultureInfo.InvariantCulture) : "None";
+            ImportedBacklog.NotifTime = NotifTime;
+            Backlogs.Add(ImportedBacklog);
+            SaveData.GetInstance().SaveSettings(Backlogs);
+            await SaveData.GetInstance().WriteDataAsync(m_signedIn);
+            NavToMainPageFunc();
+        }
+
+        public void CancelImport()
+        {
+            NavToMainPageFunc();
+        }
+    }
+}
--- backlog/Views/ImportBacklog.xaml.cs
+++ backlog/Views/ImportBacklog.xaml.cs
@@ -1,27 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+﻿using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
-using Newtonsoft.Json;
-using backlog.Models;
-using System.Collections.ObjectModel;
-using backlog.Utils;
-using System.Net.NetworkInformation;
-using backlog.Saving;
-using System.Globalization;
-using Windows.Storage;
-using Windows.UI.Xaml.Media.Imaging;
-using System.Diagnostics;
+using backlog.ViewModels;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -32,115 +11,21 @@ namespace backlog.Views
     /// </summary>
     public sealed partial class ImportBacklog : Page
     {
-        ObservableCollection<Backlog> backlogs { get; set; }
-        Backlog importedBacklog;
-        bool signedIn;
-        bool isNetworkAvailable = false;
+        public ImportBacklogViewModel ViewModel { get; set; } = new ImportBacklogViewModel();
         public ImportBacklog()
         {
             this.InitializeComponent();
-            signedIn = Settings.IsSignedIn;
-            isNetworkAvailable = NetworkInterface.GetIsNetworkAvailable();
-            importedBacklog = new Backlog();
+            ViewModel.NavToMainPageFunc = NavigateToMainPage;
         }
 
         protected async override void OnNavigatedTo(NavigationEventArgs e)
         {
             string fileName = e.Parameter as string;
-            if (fileName != null && fileName != "")
-            {
-                ProgBar.Visibility = Visibility.Visible;
-                if (isNetworkAvailable)
-                {
-                    await SaveData.GetInstance().ReadDataAsync(signedIn);
-                    backlogs = SaveData.GetInstance().GetBacklogs();
-                    StorageFolder tempFolder = ApplicationData.Current.TemporaryFolder;
-                    StorageFile file = await tempFolder.GetFileAsync(fileName);
-                    string json = await FileIO.ReadTextAsync(file);
-                    importedBacklog = JsonConvert.DeserializeObject<Backlog>(json);
-                    Debug.WriteLine(json);
-                    titleText.Text = importedBacklog.Name;
-                    directorText.Text = importedBacklog.Director;
-                    typeText.Text = importedBacklog.Type;
-                    BitmapImage image = new BitmapImage(new Uri(importedBacklog.ImageURL));
-                    coverImg.Source = image;
-                }
-                ProgBar.Visibility = Visibility.Collapsed;
-            }
+            await ViewModel.LoadBacklogFromFileAsync(fileName);
             base.OnNavigatedTo(e);
         }
 
-        private async void ImportButton_Click(object sender, RoutedEventArgs e)
-        {
-            ProgBar.Visibility=Visibility.Visible;
-            if(!isNetworkAvailable && signedIn)
-            {
-                ContentDialog contentDialog = new ContentDialog
-                {
-                    Title = "No internet",
-                    Content = "You need to be connected to the internet for this!",
-                    CloseButtonText = "Ok"
-                };
-                await contentDialog.ShowAsync();
-                return;
-            }
-            if (datePicker.SelectedDates.Count > 0)
-            {
-                if (timePicker.Time == TimeSpan.Zero)
-                {
-                    ContentDialog contentDialog = new ContentDialog
-                    {
-                        Title = "Invalid date and time",
-                        Content = "Please pick a time!",
-                        CloseButtonText = "Ok"
-                    };
-                    await contentDialog.ShowAsync();
-                    return;
-                }
-                string date = datePicker.SelectedDates[0].ToString("D", CultureInfo.InvariantCulture);
-                DateTimeOffset dateTime = DateTimeOffset.Parse(date, CultureInfo.InvariantCulture).Add(timePicker.Time);
-                int diff = DateTimeOffset.Compare(dateTime, DateTimeOffset.Now);
-                if (diff < 0)
-                {
-                    ContentDialog contentDialog = new ContentDialog
-                    {
-                        Title = "Invalid date and time",
-                        Content = "The date and time you've chosen are in the past!",
-                        CloseButtonText = "Ok"
-                    };
-                    await contentDialog.ShowAsync();
-                    return;
-                }
-            }
-            else if (timePicker.Time != TimeSpan.Zero)
-            {
-                if (datePicker.SelectedDates.Count <= 0)
-                {
-                    ContentDialog contentDialog = new ContentDialog
-                    {
-                        Title = "Invalid date and time",
-                        Content = "Please pick a date!",
-                        CloseButtonText = "Ok"
-                    };
-                    await contentDialog.ShowAsync();
-                    return;
-                }
-            }
-            importedBacklog.id = Guid.NewGuid();
-            importedBacklog.UserRating = 0;
-            importedBacklog.Progress = 0;
-            importedBacklog.CompletedDate = null;
-            importedBacklog.IsComplete = false;
-            importedBacklog.CreatedDate = DateTimeOffset.Now.ToString("D", CultureInfo.InvariantCulture);
-            importedBacklog.TargetDate = datePicker.SelectedDates.Count > 0 ? datePicker.SelectedDates[0].ToString("D", CultureInfo.InvariantCulture) : "None";
-            importedBacklog.NotifTime = timePicker.Time;
-            backlogs.Add(importedBacklog);
-            SaveData.GetInstance().SaveSettings(backlogs);
-            await SaveData.GetInstance().WriteDataAsync(signedIn);
-            Frame.Navigate(typeof(MainPage));
-         }
-
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        private void NavigateToMainPage()
         {
             Frame.Navigate(typeof(MainPage));
         }
