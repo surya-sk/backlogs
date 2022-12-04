ab64b7231dc86a012bd2e3597779d91ff226a554
surya-sk <surya.sk05@outlook.com>
2022-12-04 10:45:46 +0530

    Move Constants to new project

---
+++ Backlogs/Constants/MSALConstants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backlogs.Constants
+{
+    public static class MSALConstants
+    {
+        public static readonly string[] Scopes = new string[]
+        {
+             "user.read",
+             "Files.Read",
+             "Files.Read.All",
+             "Files.ReadWrite",
+             "Files.ReadWrite.All"
+        };
+        public static readonly string MSGraphURL = "https://graph.microsoft.com/v1.0/";
+    }
+}
---
+++ Backlogs/Constants/SettingsConstants.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backlogs.Constants
+{
+    public static class SettingsConstants
+    {
+        public const string IsFirstRun = nameof(IsFirstRun);
+        public const string ShowWhatsNew = nameof(ShowWhatsNew);
+        public const string Version = nameof(Version);
+        public const string IsSignedIn = nameof(IsSignedIn);
+        public const string UserName = nameof(UserName);
+        public const string ShowLiveTile = nameof(ShowLiveTile);
+        public const string SortOrder = nameof(SortOrder);
+        public const string CompletedSortOrder = nameof(CompletedSortOrder);
+        public const string TileStyle = nameof(TileStyle);
+        public const string TileContent = nameof(TileContent);
+        public const string AutoplayVideos = nameof(AutoplayVideos);
+        public const string AppTheme = nameof(AppTheme);
+        public const string LastCrashLog = nameof(LastCrashLog);
+
+        public static IReadOnlyDictionary<string, object> Defaults { get; } = new Dictionary<string, object>()
+        {
+            {IsFirstRun, true},
+            {ShowWhatsNew, false },
+            {Version, null },
+            {IsSignedIn, false },
+            {UserName, null },
+            {ShowLiveTile, true },
+            {SortOrder, "Name" },
+            {CompletedSortOrder, "Name" },
+            {TileStyle, "Peeking" },
+            {TileContent, "Recently Created" },
+            {AutoplayVideos, false },
+            {AppTheme, "System" },
+            {LastCrashLog, null }
+        };
+    }
+}
