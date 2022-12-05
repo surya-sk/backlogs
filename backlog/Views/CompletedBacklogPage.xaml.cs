e70f864c325817022e35e6e2450d567331af508c
surya-sk <surya.sk05@outlook.com>
2022-12-05 20:11:52 +0530

    Show Backlog details in CompletedBacklog page

---
+++ backlog/Views/CompletedBacklogPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using Backlogs.Core.ViewModels;
+using Backlogs.Services;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Backlogs.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class CompletedBacklogPage : Page
+    {
+        private CompletedBacklogViewModel ViewModel { get; set; }
+        public CompletedBacklogPage()
+        {
+            this.InitializeComponent();
+
+            var view = SystemNavigationManager.GetForCurrentView();
+            view.AppViewBackButtonVisibility = AppViewBackButtonVisibility.Disabled;
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            Guid selectedId = (Guid)e.Parameter;
+            ViewModel = new CompletedBacklogViewModel(selectedId, App.Services.GetRequiredService<INavigation>(),
+                App.Services.GetRequiredService<IUserSettings>());
+            base.OnNavigatedTo(e);
+            ConnectedAnimation imageAnimation = ConnectedAnimationService.GetForCurrentView().GetAnimation("cover");
+            imageAnimation?.TryStart(img);
+        }
+    }
+}
