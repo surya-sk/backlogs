9a99904e5201e21f44f7370fad5a33bc633ef1b6
surya-sk <surya.sk05@outlook.com>
2023-01-05 19:57:41 -0800

    Update main page UI

--- Backlogs.UWP/Views/MainPage.xaml.cs
+++ Backlogs.UWP/Views/MainPage.xaml.cs
@@ -60,14 +60,5 @@ namespace Backlogs.Views
             }
             await ViewModel.SetupProfile();
         }
-
-        private void Hub_SectionHeaderClick(object sender, HubSectionHeaderClickEventArgs e)
-        {
-            if (e.Section.Name == recentlyAddedSection.Name)
-                ViewModel.NavigateToBacklogsPage();
-            else
-                ViewModel.NavigateToCompletedPage();
-            
-        }
     }
 }
