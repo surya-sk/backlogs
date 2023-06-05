4ea7a67a1fff1449e16e0de3412620f89cf701f7
surya-sk <surya.sk05@outlook.com>
2023-06-04 21:45:05 -0400

    Add WASM and Android heads

---
+++ Backlogs.Uno/Backlogs.Uno/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+namespace Backlogs.Uno
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
